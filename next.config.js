3dc93167ffe959edbbf1efa5641d53ad5e7fc186
bahaa <bahaa.tuffaha@yahoo.com>
2023-07-13 15:16:54 +0300

    export settings

--- next.config.js
+++ next.config.js
@@ -1,12 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {  async redirects() {
-    return [
-      {
-        source: "/",
-        destination: '/programming',
-        permanent: true,
-      },
-    ]
-  },}
+  return [
+    {
+      source: '/',
+      destination: '/programming',
+      permanent: true,
+    },
+  ];
+},
+output: 'export',
+images: {
+  unoptimized: true,
+},
+};
 
 module.exports = nextConfig
